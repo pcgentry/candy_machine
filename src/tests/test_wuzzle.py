f2b10658a2bb42c2248542f9321c0d23cc31110c
Phillip Gentry <gentry@coffeemeetsbagel.com>
2021-04-28 12:54:21 -0600

    pytest

--- src/tests/test_wuzzle.py
+++ src/tests/test_wuzzle.py
@@ -1,15 +1,12 @@
-from wuzzle import Wuzzle 
+def test_wuzzle_has_name(RandomWuzzle):
+    assert RandomWuzzle.name, "Wuzzle has not named themself."
 
-class TestWuzzle():
-    def setup(self):
-        self.test_wuzzle = Wuzzle()
-        
-    def test_wuzzle_has_name(self):
-        self.setup()
-        assert self.test_wuzzle.name, "Wuzzle has not named themself."
+def test_wuzzle_init_flavor_preferences(RandomWuzzle):
+    assert RandomWuzzle.flavor_preferences, "Wuzzle has no flavor preferences"
+    assert isinstance(RandomWuzzle.flavor_preferences, dict), "Somethings wrong. Wuzzle flavor preferences are not a Dictionary"
+    assert len(RandomWuzzle.flavor_preferences) > 0, "Wuzzle has zero flavor preferences"
 
-    def test_wuzzle_has_flavor_preferences(self):
-        self.setup()
-        assert self.test_wuzzle.flavor_preferences, "Wuzzle has no flavor preferences"
-        assert type(self.test_wuzzle.flavor_preferences) is list, "Wuzzle flavor preferences is not a list"
-        assert len(self.test_wuzzle.flavor_preferences) > 0, "Wuzzle has zero flavor preferences"
+    for key, value in RandomWuzzle.flavor_preferences.items():
+
+        assert isinstance(key, str), "The first part of a Wuzzle pref should be the flavor"
+        assert isinstance(value, float), "The second part of a Wuzzle pref should be the likelihood of eating"
--- src/wuzzle.py
+++ src/wuzzle.py
@@ -1,9 +1,15 @@
 from faker import Faker
+import numpy as np
 
 class Wuzzle():
     '''
     Creates a wuzzle critter to eat CANDY.
     '''
     def __init__(self):
-      fake = Faker()
-      self.name = fake.name()
+      self.fake = Faker()
+      self.flavors = ['apple', 'toenail', 'earwax', 'green tea', 'peach', 'tarball', 'toe fuzz']
+      self.name = self.fake.name()
+      self.generate_flavor_preferences()
+
+    def generate_flavor_preferences(self):
+      self.flavor_preferences = {'apple': .6}
