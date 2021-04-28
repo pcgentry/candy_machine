520f3c6e93bf826c069e128b141ef7239168605d
Phillip Gentry <gentry@coffeemeetsbagel.com>
2021-04-28 07:27:02 -0600

    more tests

--- src/tests/test_wuzzle.py
+++ src/tests/test_wuzzle.py
@@ -8,3 +8,8 @@ class TestWuzzle():
         self.setup()
         assert self.test_wuzzle.name, "Wuzzle has not named themself."
 
+    def test_wuzzle_has_flavor_preferences(self):
+        self.setup()
+        assert self.test_wuzzle.flavor_preferences, "Wuzzle has no flavor preferences"
+        assert type(self.test_wuzzle.flavor_preferences) is list, "Wuzzle flavor preferences is not a list"
+        assert len(self.test_wuzzle.flavor_preferences) > 0, "Wuzzle has zero flavor preferences"
