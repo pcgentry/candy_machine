6843f53e74435054948f17231719191b498b6b56
Phillip Gentry <gentry@coffeemeetsbagel.com>
2021-05-06 21:46:50 -0500

    more graphs

--- src/config.py
+++ src/config.py
@@ -10,9 +10,9 @@ WORLD = {
   "cookbook_page_length": 3,
   "cookbook_num_pages": 2,
   "menu_size": 3,
-  "initial_wuzzle_population": 20,
-  "initial_candy_population": 20,
-  "hunger_rate": .25,
+  "initial_wuzzle_population": 40,
+  "initial_candy_population": 40,
+  "hunger_rate": .2,
   "hunger_death": .75,
 }
 
